d8e3ce54d94360d772665230f6a42e80eb91d74b
João Vitor Torresan <56739230+JVTE@users.noreply.github.com>
2023-06-16 16:19:37 -0300

    Adicionando um relógio e a data atual com o JS

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time(){
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+
+    if (min < 10) min = '0' + min;
+
+    if (s < 10) s = '0' + s;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = s;
+})
+
+//Cria um objeto Date com a data e hora atuais
+var dataAtual = new Date();
+
+//Extrai os componentes da data atual
+var dia = dataAtual.getDate();
+var mes = dataAtual.getMonth() + 1;
+var ano = dataAtual.getFullYear();
+
+var dataFormatada = dia + '/' + mes + '/' + ano;
+
+document.getElementById("dataAtual").innerHTML = dataFormatada;
