85837b7a6b229c717e501f01a060c54199faef16
João Vitor Torresan <56739230+JVTE@users.noreply.github.com>
2023-07-06 17:25:10 -0300

    arrumando a posição da img e da hora/data e comentando

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,35 @@
 // Cria um alerta quando a página é gerada
-function alerta() {
-    alert('Cancelamentos de reunião com a Ana DA RECEPÇÃO ou em seu e-mail, caso tenha alguma duvida FAVOR verificar o RODAPÉ da página')
- }
+// function alerta() {
+//     alert('Cancelamentos de reunião com a Ana DA RECEPÇÃO ou em seu e-mail, caso tenha alguma duvida FAVOR verificar o RODAPÉ da página')
+//  }
 
- const horas = document.getElementById('horas');
- const minutos = document.getElementById('minutos');
- const segundos = document.getElementById('segundos');
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
 
- const relogio = setInterval(function time(){
-     let dateToday = new Date();
-     let hr = dateToday.getHours();
-     let min = dateToday.getMinutes();
-     let s = dateToday.getSeconds();
+const relogio = setInterval(function time(){
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
 
-     if (hr < 10) hr = '0' + hr;
+    if (hr < 10) hr = '0' + hr;
 
-     if (min < 10) min = '0' + min;
-     if (s < 10) s = '0' + s;
+    if (min < 10) min = '0' + min;
+    if (s < 10) s = '0' + s;
 
-     horas.textContent = hr;
-     minutos.textContent = min;
-     segundos.textContent = s;
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = s;
  })
 
 //Cria um objeto Date com a data e hora atuais
- var dataAtual = new Date();
+var dataAtual = new Date();
 
- //Extrai os componentes da data atual
- var dia = dataAtual.getDate();
+//Extrai os componentes da data atual
+var dia = dataAtual.getDate();
 var mes = dataAtual.getMonth() + 1;
- var ano = dataAtual.getFullYear();
+var ano = dataAtual.getFullYear();
 
 var dataFormatada = dia + '/' + mes + '/' + ano;
 
